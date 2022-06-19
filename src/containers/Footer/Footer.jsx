86ec9b29396c1b5090cc090ea889df245028910a
Gavin Stirling <gavinstir@gmail.com>
2022-06-19 22:59:49 +0100

    Completed setup of the footer component and entire website

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
 
-import "./Footer.scss"
+import "./Footer.scss";
+
+import githubLogo from "../../assets/images/footer/github-logo.png";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <footer className="footer">
+            <div className="footer__links">
+                <a
+                    className="footer__link"
+                    href={"https://github.com/GavinStirling"}
+                >
+                    <img src={githubLogo} alt="Github Logo Link" />
+                </a>
+                <a
+                    className="footer__link"
+                    href="https://www.linkedin.com/in/gavin-stirling-646b14146"
+                >
+                    in
+                </a>
+                <a className="footer__link" href="/#">
+                    CV
+                </a>
+            </div>
+            <p className="footer__rights">All Right's Reserved, 2022</p>
+            <p className="footer__email">gavinstir@gmail.com</p>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
