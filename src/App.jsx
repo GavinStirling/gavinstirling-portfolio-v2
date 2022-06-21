b883eeef3a2fa3949cb67f654c2489661809dfcc
Gavin Stirling <gavinstir@gmail.com>
2022-06-21 14:47:13 +0100

    Added useState to App.js for switching between the project and skill pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,43 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import './App.scss';
-import './assets/sass/reset.scss'
+import "./App.scss";
+import "./assets/sass/reset.scss";
 
-import Nav from "./containers/Nav/Nav"
-import Intro from './containers/Intro/Intro';
-import Skills from './containers/Skills/Skills';
-import Projects from './containers/Projects/Projects';
-import About from './containers/About/About';
-import Footer from './containers/Footer/Footer';
+import Nav from "./containers/Nav/Nav";
+import Intro from "./containers/Intro/Intro";
+import Skills from "./containers/Skills/Skills";
+import Projects from "./containers/Projects/Projects";
+import About from "./containers/About/About";
+import Footer from "./containers/Footer/Footer";
 
 function App() {
-  const [switchProjectsPage, setSwitchProjectsPage] = useState(false);
+    const [switchProjectsPage, setSwitchProjectsPage] = useState(false);
+    const [switchSkillsPage, setSwitchSkillsPage] = useState(false);
 
-  return (
-    <div className="App">
+    const handleProjectsPageClick = () => {
+        if (!switchProjectsPage) {
+            setSwitchProjectsPage(true);
+        }
+        setSwitchProjectsPage(false);
+    };
 
-      <Nav />
-      <Intro />
-      <Skills />
-      <Projects />
-      <About />
-      <Footer />
-      
-    </div>
-  );
+    const handleSkillsPageClick = () => {
+        if (!switchProjectsPage) {
+            setSwitchProjectsPage(true);
+        }
+        setSwitchProjectsPage(false);
+    };
+
+    return (
+        <div className="App">
+            <Nav />
+            <Intro />
+            <Skills />
+            <Projects />
+            <About />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
